682bb53e629e3d8cafbd645c26bdf8c032a8ddc7
Fabricio Bezerra <contato@adawn.dev>
2019-12-03 17:30:58 -0300

    Day 2

---
+++ day2/part_1.rb
@@ -0,0 +1,22 @@
+text = File.open(__dir__ + '/input.txt').read
+
+input = text.chomp.split(',').map(&:to_i)
+
+input[1] = 12
+input[2] = 2
+
+input.each_slice(4) do |op_code, pos_x, pos_y, pos_result|
+  if op_code == 99
+    break
+  elsif op_code == 1
+    x = input[pos_x]
+    y = input[pos_y]
+    input[pos_result] = x + y
+  elsif op_code == 2
+    x = input[pos_x]
+    y = input[pos_y]
+    input[pos_result] = x * y
+  end
+end
+
+pp input

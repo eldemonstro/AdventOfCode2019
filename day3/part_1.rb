a96fc80b180600f732955a7365f7923865c9ad9f
Fabricio Bezerra <contato@adawn.dev>
2019-12-04 19:23:35 -0300

    Day 3

---
+++ day3/part_1.rb
@@ -0,0 +1,44 @@
+text = File.open(__dir__ + '/input.txt').read
+
+wires = text.split(/\n/)
+
+intersections = {}
+paths = {}
+
+wires.each do |wire|
+  directions = wire.split(',')
+  position = [0, 0]
+
+  directions.each do |direction|
+    intention, distance = direction.split('', 2)
+    distance = distance.to_i
+
+    until distance == 0 do
+      case intention
+      when 'U'
+        position[1] += 1
+      when 'D'
+        position[1] -= 1
+      when 'L'
+        position[0] -= 1
+      when 'R'
+        position[0] += 1
+      end
+
+      if paths["#{position[0]},#{position[1]}"]
+        intersections["#{position[0]},#{position[1]}"] = position[0], position[1]
+      else
+        paths["#{position[0]},#{position[1]}"] = true
+      end
+
+      distance -= 1
+    end
+  end
+end
+
+result = intersections.values.inject(1 << 64) do |closest, intersection|
+  distance = intersection[0].abs + intersection[1].abs
+  closest > distance ? distance : closest
+end
+
+puts result
---
+++ day3/part_2.rb
@@ -0,0 +1,43 @@
+text = File.open(__dir__ + '/input.txt').read
+
+wires = text.split(/\n/)
+
+paths = {}
+fewest_steps = []
+
+wires.each_with_index do |wire, index|
+  directions = wire.split(',')
+  position = [0, 0]
+  step_count = 0
+
+  directions.each do |direction|
+    intention, distance = direction.split('', 2)
+    distance = distance.to_i
+
+    until distance == 0 do
+      case intention
+      when 'U'
+        position[1] += 1
+      when 'D'
+        position[1] -= 1
+      when 'L'
+        position[0] -= 1
+      when 'R'
+        position[0] += 1
+      end
+
+      if paths["#{position[0]},#{position[1]},#{index - 1}"]
+        fewest_steps << paths["#{position[0]},#{position[1]},#{index-1}"][:step_count] + step_count
+      end
+
+      paths["#{position[0]},#{position[1]},#{index}"] = { step_count: step_count }
+
+      distance -= 1
+      step_count += 1
+    end
+  end
+end
+
+# + 2 comes from the first step (from pos 0, 0)
+pp fewest_steps.min + 2
+
